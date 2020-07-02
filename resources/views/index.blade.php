daf4d50e6d981900be70afb294eebddf324e04d2
Felipe Almonacid <felipe.almonacidgallardo@gmail.com>
2020-07-02 00:18:29 -0400

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,14 +49,14 @@
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                            <h3>{{ $customerQuantity }}</h3>
 
-                            <p>Bounce Rate</p>
+                            <p>Clientes en el sistema</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('customers.index') }}" class="small-box-footer">Mas info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
